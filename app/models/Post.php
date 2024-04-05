41b4d91e7f5f2d8e5bab5d1fc05f386818028bbb
mjones <you@example.com>
2024-04-05 17:02:15 -0400

    'main'

--- app/models/Post.php
+++ app/models/Post.php
@@ -5,31 +5,52 @@ namespace app\models;
 class Post
 {
     //todo make methods here
-    //$db = new PDOdb('localhost', 'root','root','homework_9');
     public function getAllPosts()
     {
-        $query=("SELECT * FROM posts WHERE  id");
-        return $query;
+        $db = \Config\Database::connect();
+        $query = $db->query("SELECT * FROM posts");
+        return $query->getResultArray();
+
     }
     public function getPostById($id)
     {
-        $query =("SELECT * FROM posts WHERE id = :id");
-        return $query;
+        $db = \Config\Database::connect();
+        $query = $db->query("SELECT * FROM posts WHERE id = $id");
+        return $query->getResultArray();
+        
     }
     public function savePost($id)
     {
-        $query = "INSERT INTO posts (title, description) VALUES (:title, :description)";
-        return $query;
+        $db = \Config\Database::connect();
+        $query = $db->query("INSERT INTO posts (title, description) VALUES (:title, :description)");
+        $data = [
+            'title' => $id['title'],
+            'description' => $id['description'],
+        ];
+        $query->execute($data);
+       
     }
     public function updatePost($id)
     {
-        $query = "UPDATE posts SET title = :title, description = :description WHERE id = :id";
-        return $query;
+    $db = \Config\Database::connect();
+    $query = $db->query("UPDATE posts SET title = :title, description = :description WHERE id = $id");
+    $data = [
+        'title' => $id['title'],
+        'description' => $id['description'],
+    ];
+    $query->execute($data);
+       
     }
     public function deletePost($id)
-    {
-        $query = "DELETE FROM posts WHERE id = :id";
-        return $query;
+    {   
+        $db = \Config\Database::connect();
+        $query = $db->query("DELETE FROM posts WHERE id = $id");
+        $data = [
+            'title' => $id['title'],
+            'description' => $id['description'],
+        ];
+        $query->execute($data);
+       
     }
 
 
